5bd63bbdb248db44b02de92bcdf141c770f04cd2
Ulver Pedersen <ulpe@ek.dk>
2026-03-27 13:45:01 +0000

    Ændret fra produkter til vindmøller

--- test_app.py
+++ test_app.py
@@ -7,14 +7,14 @@ def client():
     with app.test_client() as client:
         yield client
 
-def test_hent_produkter(client):
-    response = client.get('/api/produkter')
+def test_hent_vindmoeller(client):
+    response = client.get('/api/vindmoeller')
     assert response.status_code == 200
 
-def test_hent_enkelt_produkt(client):
-    response = client.get('/api/produkter/1')
+def test_hent_enkelt_vindmoelle(client):
+    response = client.get('/api/vindmoeller/1')
     assert response.status_code == 200
 
-def test_produkt_ikke_fundet(client):
-    response = client.get('/api/produkter/999')
+def test_vindmoelle_ikke_fundet(client):
+    response = client.get('/api/vindmoeller/999')
     assert response.status_code == 404
